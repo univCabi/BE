f5db327db54fc0b94b880dd74c05d006c309e6b5
yemin4242 <whereismysejong>
2024-09-22 03:30:36 +0900

    Feature: Logout urls + views

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('login/', views.LoginView.as_view() , name='login'),
-
+    path('logout/', views.LogoutView.as_view() , name='logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -6,3 +6,7 @@ from django.views import View
 class LoginView(View):
     def get(self, request):
         return HttpResponse('Login')
+    
+class LogoutView(View):
+    def get(self, request):
+        return HttpResponse('Logout')
