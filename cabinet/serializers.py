11fa55360191f76381b5e0c6100221240494e875
yemin4242 <whereismysejong>
2024-11-27 20:13:48 +0900

    Fix: Cabinet main api return right isMine field value

--- cabinet/serializers.py
+++ cabinet/serializers.py
@@ -26,10 +26,14 @@ class CabinetInfoSerializer(serializers.ModelSerializer):
     def get_username(self, obj):
         return obj.user_id.name if obj.user_id else None
 
-    #TODO: Impelment this method
+    #TODO: Implement this method
     def get_isMine(self, obj):
         request = self.context.get('request')
-        user_id = users.objects.get(id=request.user.id).id
+        user_id = cabinets.objects.filter(id=obj.id).values('user_id').first().get('user_id')
+
+        print("user_id: ", user_id)
+        print("request.user.id: ", request.user.id)
+
         return user_id == request.user.id if user_id else False
 
     
